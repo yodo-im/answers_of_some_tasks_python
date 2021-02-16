feacadb1a0047c266f77109f89e6fce6625070f0
yodo-im <78910536+yodo-im@users.noreply.github.com>
2021-02-15 23:13:33 -0800

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,11 @@
+m = int(input("Введите массу\n"))
+h = int(input("Введите высоту\n"))
+G = 6.67 * 10**(-11)
+M = 6*10**24
+R = 6371 * 10**3
+Ft = G * ((M * m)/((R + h)**2))
+Ft = str(Ft)
+for i in range(0,len(Ft)):
+    if Ft[i] == '.':
+        break
+print(Ft[0:i])
